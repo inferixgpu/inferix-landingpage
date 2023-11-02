0d5373043b65b480597160750f4b7c49751993f3
h3d-anhntl <anhntl@house3d.net>
2023-11-02 17:15:39 +0700

    update account mail, add button Register

--- src/routes/teaser_api/+server.js
+++ src/routes/teaser_api/+server.js
@@ -5,15 +5,15 @@ export async function GET(params) {
 		const data = JSON.stringify({
 			Messages: [
 				{
-					From: { Email: 'leo.kim@vloclabs.com', Name: 'Inferix Supporter' },
-					To: [{ Email: 'official@inferix.io', Name: email }],
+					From: { Email: 'dev@inferix.io', Name: 'Inferix Supporter' },
+					To: [{ Email: 'dev@inferix.io', Name: email }],
 					Subject: 'Sign up for Exclusive Early Access',
 					TextPart: email
 				}
 			]
 		});
 
-		const basicAuth = btoa(`660e9d06d3855f360c149853c6fcab58:862bceefeae3d74631af2ce960953f7a`);
+		const basicAuth = btoa(`0554438622b88c21de77d0174e0107be:e03e6ea85bc46612db0aba0bf7902f9f`);
 		await fetch('https://api.mailjet.com/v3.1/send', {
 			method: 'POST',
 			body: data,
