c041fde99201b1aa3aaefd9a00e85e6d687f0f44
h3d-anhntl <anhntl@house3d.net>
2023-12-14 10:57:33 +0700

    fix sai chính tả phần About

--- src/components/body/Body.svelte
+++ src/components/body/Body.svelte
@@ -55,7 +55,7 @@
 				<p
 					class="md:text-left text-center text-xs md:text-[1.75rem] font-pre font-normal leading-default md:text-white text-textMobile"
 				>
-					An innovated platform for 3D rendering and AI referencing using crowdsourced GPUs globally
+					An innovated platform for 3D rendering and AI inference using crowdsourced GPUs globally
 				</p>
 			</div>
 		</Saos>
