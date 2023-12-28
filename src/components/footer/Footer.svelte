34c4ccd659d03b4f8b9ac5e25c2d8aad277b0e25
TienVinh <s2gali2s@gmail.com>
2023-12-28 15:29:51 +0700

    tienmvv.Update link header logo inferix

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -63,7 +63,7 @@
         <div
                 class="flex md:gap-4 gap-2 md:order-1 order-2 md:flex-row flex-col items-center md:items-start mt-8 md:mt-0"
         >
-            <a href="https://inferix.io/">
+            <a href="/">
                 <img src={HeaderLogo} alt="logo" class="w-[152px] h-[32px]"/>
             </a>
             <p class="font-base font-outfit font-normal mt-4 md:mt-0 text-white whitespace-nowrap">
