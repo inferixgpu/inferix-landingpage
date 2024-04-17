1df922e0594e7acad45fd742c6b612f6c95fbbdb
ssm-namdd <namdd@saseeme.com>
2024-04-17 17:24:59 +0700

    NamDD.IN-706 relayout industry

---
+++ src/components/body/Indus.svelte
@@ -0,0 +1,186 @@
+<script lang="ts">
+	import ArrowDown from '$images/icons/ArrowDown.svg';
+	import ArrowDownMobile from '$images/icons/ArrowDownMobile.svg';
+	import EllipseBackground from '$images/png/EllipseBackground.png';
+	import In1 from '$images/png/industries/1.png';
+	import In2 from '$images/png/industries/2.png';
+	import In3 from '$images/png/industries/3.png';
+	import In4 from '$images/png/industries/4.png';
+	import In5 from '$images/png/industries/5.png';
+	import Saos from 'saos';
+	import IndustryListDesktop from './IndustryListDesktop.svelte';
+	import industry_line from '$images/icons/industry_line.svg';
+
+	let screenSize: number;
+</script>
+
+<svelte:window bind:innerWidth={screenSize} />
+
+<div class="industry pb-[48px] mx-auto w-100% container mt-[160px] div-room">
+	<div><img src={industry_line} alt="industry_line" /></div>
+	<div class="industry-content">
+		<div>
+			<div><div>Industries</div></div>
+			<div class="in-images">
+				<img src={In1} alt="image1" />
+				<div>
+					<div>Entertainment & Media</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="in-images">
+				<img src={In2} alt="image2" />
+				<div>
+					<div>Architecture & Design</div>
+				</div>
+			</div>
+			<div class="in-images">
+				<img src={In3} alt="image3" />
+				<div>
+					<div>Animation & VFX</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="in-images">
+				<img src={In4} alt="image4" />
+				<div>
+					<div>AR & VR</div>
+				</div>
+			</div>
+			<div class="in-images">
+				<img src={In5} alt="image5" />
+				<div>
+					<div>Game Content</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.industry {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		width: 1199px;
+	}
+
+	.industry > div:first-child {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.industry-content {
+		position: absolute;
+		top: 131px;
+		left: 153px;
+		width: 894px;
+		height: 861px;
+		display: flex;
+	}
+
+	.industry-content > div {
+		width: 282px;
+		padding: 1px;
+	}
+
+	.industry-content > div:first-child {
+		padding: 10px 0 113px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.industry-content > div:first-child > div:first-child {
+		& > div {
+			text-align: center;
+			font-size: 32px;
+			font-weight: 600;
+			line-height: 48px; /* 150% */
+			background: linear-gradient(180deg, rgba(255, 255, 255, 0.3) 8.85%, #fff 100%);
+			background-clip: text;
+			-webkit-background-clip: text;
+			-webkit-text-fill-color: transparent;
+			transform: translate(-68px, -14px);
+		}
+	}
+
+	.in-images {
+		width: 100%;
+		height: 362px;
+		position: relative;
+		border: 1px solid rgba(244, 244, 244, 0.3);
+
+		& > img {
+			width: 100%;
+			height: 100%;
+		}
+
+		& > div {
+			width: 100%;
+			height: 69px;
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			background: linear-gradient(
+				180deg,
+				rgba(49, 91, 91, 0) -1.11%,
+				rgba(49, 86, 91, 0.44) 23.83%,
+				#31595b 99.56%
+			);
+			stroke-width: 1px;
+			stroke: rgba(255, 255, 255, 0);
+			backdrop-filter: blur(10px);
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-top: 1px solid rgba(255, 255, 255, 0.3);
+
+			& > div {
+				color: #fff;
+				font-feature-settings: 'clig' off, 'liga' off;
+				font-size: 16px;
+				font-weight: 600;
+				line-height: 36px; /* 225% */
+			}
+		}
+	}
+
+	.industry-content > div:nth-child(2) {
+		padding: 93px 0 0 0;
+		margin-left: 24px;
+		display: flex;
+		flex-direction: column;
+		gap: 24px;
+	}
+
+	.industry-content > div:nth-child(3) {
+		margin-left: 23px;
+		margin-top: -1px;
+		width: 283px;
+		display: flex;
+		flex-direction: column;
+		gap: 24px;
+	}
+
+	.rooms {
+		animation: textMove 5s linear infinite;
+	}
+	.div-room {
+		overflow: hidden;
+	}
+	@keyframes textMove {
+		0% {
+			/* left: 0; */
+			transform: translateX(200%);
+		}
+		100% {
+			/* left: -100%; */
+			transform: translateX(-200%);
+		}
+	}
+</style>
