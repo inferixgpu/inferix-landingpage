972424778e6feffb3a3f2254c8883f3e2961b8f3
ssm-namdd <namdd@saseeme.com>
2024-04-17 15:19:37 +0700

    NamDD.IN-705 update proven technologies layout

--- src/components/body/Body.svelte
+++ src/components/body/Body.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Frame from '$images/png/Frame.png';
- 	import innovatedHeader from '$images/png/inferixInnovateHeader.png';
+	import innovatedHeader from '$images/png/inferixInnovateHeader.png';
 	import Saos from 'saos';
 	import Industries from './Industries.svelte';
 	import RoadMapDesktop from './RoadMapDesktop.svelte';
@@ -17,8 +17,8 @@
 	import Network3 from '$images/png/Network3.png';
 	import SystemArchitecture from '$components/body/SystemArchitecture.svelte';
 	import RoadMapMobile from '$components/body/RoadMapMobile.svelte';
-	import ProvenTechnologies from '$components/body/NewProven.svelte';
-	import InferixIntro from "$videos/-6832-41e3-b4e4-f39f89c47ac3.mp4";
+	import ProvenTechnologies from '$components/body/ProvenTechnologies.svelte';
+	import InferixIntro from '$videos/-6832-41e3-b4e4-f39f89c47ac3.mp4';
 
 	let screenSize: number;
 
@@ -83,7 +83,7 @@
 <div id="about" class="bg-black">
 	<div class="innovated">
 		<div class="innovated-header">
-			<img class="w-[256px] h-[52px] mt-[680px]" src="{innovatedHeader}"/>
+			<img class="w-[256px] h-[52px] mt-[680px]" src={innovatedHeader} />
 			<div class="innovated-header-content">
 				<p>An innovated platform for 3D rendering and AI</p>
 				<p>inference using crowdsourced GPUs globally</p>
@@ -94,7 +94,7 @@
 				<source src={InferixIntro} type="video/mp4" />
 			</video>
 		</div>
-		<div class="innovated-footer"></div>
+		<div class="innovated-footer" />
 	</div>
 
 	<MpvWalkthrough />
@@ -246,17 +246,17 @@
 	}
 
 	.innovated {
-		@apply flex flex-col relative w-full ;
+		@apply flex flex-col relative w-full;
 		& > .innovated-header {
 			@apply flex flex-col h-[1200px] w-[1288px] mx-auto bg-cover bg-center -mt-[430px] items-center  gap-10 z-20;
-			background-image: url("$images/png/innovatedBackground.png");
+			background-image: url('$images/png/innovatedBackground.png');
 			& > .innovated-header-content {
 				text-align: center;
 				font-size: 40px;
 				font-style: normal;
 				font-weight: 500;
 				line-height: normal;
-				background: linear-gradient(159deg, #FFF -7.96%, rgba(255, 255, 255, 0.00) 124.67%);
+				background: linear-gradient(159deg, #fff -7.96%, rgba(255, 255, 255, 0) 124.67%);
 				background-clip: text;
 				-webkit-background-clip: text;
 				-webkit-text-fill-color: transparent;
@@ -265,7 +265,6 @@
 		& > .innovated-body {
 			@apply -mt-[350px] z-10;
 		}
-
 	}
 	@media screen and (max-width: 900px) {
 		.partnerDiv {
