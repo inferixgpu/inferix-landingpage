f14d56729d3119a81e2fbcb920cd0605840d0870
TienVinh <s2gali2s@gmail.com>
2024-04-23 11:04:00 +0700

    tienmvv.IN-734 [IN- Landingpage] Fix font-size màn hình Mobile

--- src/components/body/RoadMapMobile.svelte
+++ src/components/body/RoadMapMobile.svelte
@@ -67,7 +67,7 @@
 		<Saos once top="200" animation={'puff-in-center 0.5s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
 			<p
 				id="text-title"
-				class="md:text-center text-center font-pre font-extrabold md:text-[48px] text-[32px] mb-[80px]"
+				class="md:text-center text-center font-pre font-extrabold max-md:!text-[24px] text-[32px] mb-[80px]"
 			>
 				Roadmap
 			</p>
@@ -117,7 +117,6 @@
 	@media screen and (max-width: 768px) {
 		#text-title {
 			text-align: center;
-			font-size: 32px;
 			font-weight: 600;
 			line-height: 48px; /* 150% */
 			background: linear-gradient(180deg, rgba(255, 255, 255, 0.3) 8.85%, #fff 100%);
