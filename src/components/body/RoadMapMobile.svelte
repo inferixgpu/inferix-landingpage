130bbd74ce6222f83a66e81e5f8a2c1f1f45d6dc
ssm-namdd <namdd@saseeme.com>
2024-04-26 09:14:27 +0700

    NamDD.IN-755/style style: decrease mobile roadmap padding and font-size

--- src/components/body/RoadMapMobile.svelte
+++ src/components/body/RoadMapMobile.svelte
@@ -62,7 +62,7 @@
 
 <svelte:window bind:innerWidth={screenSize} />
 
-<div class="md:mt-[120px] mt-[120px] pb-[200px]">
+<div class="md:mt-[120px] mt-[120px] pb-[50px]">
 	<div id="roadmap" class="md:block container mx-auto w-100%">
 		<Saos once top="200" animation={'puff-in-center 0.5s cubic-bezier(0.35, 0.5, 0.65, 0.95) both'}>
 			<p
