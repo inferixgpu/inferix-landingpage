6582118fe9411ff51db5ce686036491b77bf0157
TienVinh <s2gali2s@gmail.com>
2024-08-12 14:22:48 +0700

    tienmvv.IN-1057 [ỊN-Landingpage] Update UI popup Register

--- src/components/inferix/SignUpModal/SignUpModal.svelte
+++ src/components/inferix/SignUpModal/SignUpModal.svelte
@@ -18,17 +18,17 @@
 
 {#if success}
 	<div
-		class="flex flex-col leading-default gap-[32px] justify-center items-center px-8 py-10 font-outfit"
+		class="flex flex-col leading-default gap-[32px] justify-center items-center p-0 font-outfit"
 	>
 		<img src={WelcomeImg} alt="success" width="244px" height="74px" />
-		<h2 class="text-[2rem] leading-normal font-medium text-center text-black">
+		<h2 class="text-[30px] leading-normal font-medium text-center text-black">
 			Thank you for registering!
 		</h2>
 	</div>
 {:else}
-	<div class="flex flex-col justify-center items-center px-3 py-8 font-outfit">
-		<h2 class="text-[2.5rem] leading-[28px] font-bold text-black">Sign Up For Beta</h2>
-		<span class="text-xl leading-[28px] mt-[20px] text-[#898989]">public access is coming soon</span
+	<div class="flex flex-col justify-center items-center p-0 font-outfit">
+		<h2 class="text-[30px] leading-[28px] font-bold text-black">Sign Up For Beta</h2>
+		<span class="text-[18px] leading-[28px] mt-[20px] text-[#898989]">public access is coming soon</span
 		>
 		<div class="flex mt-[56px] flex-col lg:flex-row lg:items-center gap-3 w-full">
 			<input bind:value={email} type="email" class="email-input" placeholder="Enter your e-mail" />
