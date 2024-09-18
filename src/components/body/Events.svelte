6148fb9ee47d4861bcd0c5d6427009b472a4fa59
ssm-namdd <namdd@saseeme.com>
2024-09-18 14:57:00 +0700

    NamDD.IN-LP-1142/feat/create-events-ui first commit

---
+++ src/components/body/Events.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+	let screenSize: number;
+
+</script>
+
+<svelte:window bind:innerWidth={screenSize} />
+<div class="events">
+	
+</div>
+
+<style lang="postcss">
+	
+</style>
