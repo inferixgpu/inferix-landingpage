f3fc6237235970d0f46522a59ad47d8702f39cea
ssm-namdd <namdd@saseeme.com>
2024-09-22 16:42:31 +0700

    style/fix-style-event chagne border color

--- src/components/body/Events.svelte
+++ src/components/body/Events.svelte
@@ -171,7 +171,7 @@
 					rgba(0, 214, 217, 0.2) 0%,
 					rgba(0, 192, 133, 0) 100%
 				);
-				border: 3px solid rgba(0, 214, 217, 0.2);
+				border: 3px solid #02352b;
 
 				display: flex;
 				flex-direction: column;
