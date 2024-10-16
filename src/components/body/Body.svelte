8313b6734188457c175cac27f3c1fe7994e828c7
ssm-namdd <namdd@saseeme.com>
2024-10-16 14:43:52 +0700

    fix style of partners

--- src/components/body/Body.svelte
+++ src/components/body/Body.svelte
@@ -274,7 +274,7 @@
 	<div>
 		<div
 			id="partners"
-			class="flex pb-0 max-sm:w-full lg:w-[894px] md:flex gap-[48px] flex-col mx-auto justify-center items-center md:pb-[60px] mt-[20px] md:mt-[70px]"
+			class="flex pb-0 max-sm:w-full lg:w-[894px] md:flex gap-[48px] flex-col mx-auto justify-center items-center pb-[40px] md:pb-[60px] mt-[20px] md:mt-[70px]"
 		>
 			<Saos
 				once
